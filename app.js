e7adb144e02f1a36852e82ed423e4eda33baea08
= <=>
2021-02-02 22:53:47 +0000

    stigma

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
-// const dotenv = require("dotenv");
-// dotenv.config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
